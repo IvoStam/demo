3b319c48cd135f8604ccf51c8e7586affeb8751f
IvoStam <ivelin.stamatov@realdolmen.com>
2018-11-16 16:08:50 +0100

    asdad

--- src/main/java/com/realdolmen/jenkins/domain/Greeter.java
+++ src/main/java/com/realdolmen/jenkins/domain/Greeter.java
@@ -3,6 +3,6 @@ package com.realdolmen.jenkins.domain;
 public class Greeter {
     public void greet(String name) {
 
-        System.out.println("Welcome " + name + "to Jenkins CI");
+        System.out.println("Welcome to Jenkins CI, " + name  );
     }
 }
